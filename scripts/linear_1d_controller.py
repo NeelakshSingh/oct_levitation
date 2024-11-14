[Hall Sensor Republisher][BUG] random 180 degree flips in transformed field vector Even for a constant quaternion and for the same average wand position. Several samples in the rotated field are flipped by 180 degrees. This rotation seems to be rather random. The problem does not stem from Vicon since even using a constant quaternion causes this issue. For the test dataset the wand was kept stationary and no currents were run in the coils so the field signal is pure white noise over a DC mean value. Possible causes of this flip are unknown. Numerical instabilities seem unlikely since the exact 180 degree flip is too precise to be a coincidence. There has to be some ambiguity somwhere. A detailed analysis of the time steps where the flips happen and the corresponding original and transformed field values will be analyzed in order to check what the actual culprit is. Stepping through the code manually will be considered as an option.

scripts/linear_1d_controller.py
@@ -3,19 +3,20 @@ import rospy
 import tf2_ros
 import control as ct
 import scipy.signal as signal
+import oct_levitation.controllers as controllers
 
 from time import perf_counter
 from copy import deepcopy
 
 import oct_levitation.common as common
-from oct_levitation.controllers import PID1D
 from oct_levitation.filters import SingleChannelLiveFilter, MultiChannelLiveFilter
 
 from control_utils.msg import VectorStamped
 from geometry_msgs.msg import TransformStamped
+from std_msgs.msg import Float64
 from control_utils.general.utilities_jecb import init_hardware_and_shutdown_handler
 
-HARDWARE_CONNECTED = False
+HARDWARE_CONNECTED = True
 INTEGRATOR_WINDUP_LIMIT = 100
 CLEGG_INTEGRATOR = False
 MAGNET_TYPE = "small_ring" # options: "wide_ring", "small_ring"
@@ -26,7 +27,7 @@ DIPOLE_AXIS = np.array([0, 0, 1])
 TOL = 1e-3
 
 CURRENT_MAX = 3.0 # [A]
-CURRENT_POLARITY_FLIPPED = False
+CURRENT_POLARITY_FLIPPED = True
 
 # Controller Design
 B_friction = 0.0 # friction coefficient
@@ -36,13 +37,10 @@ T_controller = 1/f_controller
 
 A = np.array([[0, 1], [0, -B_friction/m]])
 B = np.array([[0, 1/m]]).T
-C = np.array([[1, 0]]) # output is position
-Ad, Bd, Cd, Dd, Td = signal.cont2discrete((A, B, C, 0), dt=T_controller, method='zoh')
-Q = np.eye(2)
-R = 1
-K, S, E = ct.dlqr(Ad, Bd, Q, R)
-KP = K[0,0]
-KD = K[0,1]
+Q = np.diag([1, 1])
+rho = 0.1
+R = 1*rho
+Qi = np.diag([1, 1]) # Weights for error integral
 
 class Linear1DPositionPIDController:
 
@@ -52,7 +50,6 @@ class Linear1DPositionPIDController:
         rospy.init_node('linear_1d_controller', anonymous=True)
         self.current_pub = rospy.Publisher("/orig_currents", VectorStamped, queue_size=10)
         self.current_msg = VectorStamped()
-        self.Ts = 1/100
         rospy.logwarn("HARDWARE_CONNECTED is set to {}".format(HARDWARE_CONNECTED))
         init_hardware_and_shutdown_handler(HARDWARE_CONNECTED)  
         self.vicon_frame = f"vicon/{MAGNET_TYPE}_S{MAGNET_STACK_SIZE}/Origin"
@@ -64,51 +61,43 @@ class Linear1DPositionPIDController:
         
         self.dipole_mass = m
         
-        rospy.sleep(0.1) # wait for the tf listener to get the first transform
-
-
-        # self.d_filter = SingleChannelLiveFilter(
-        #     N = 3, Wn = 45,
-        #     btype='low',
-        #     analog=False,
-        #     ftype='butter',
-        #     fs = (1/self.Ts)
-        # )
-        # self.e_filter = deepcopy(self.d_filter)
-        # self.currents_filter = MultiChannelLiveFilter(
-        #     channels=8,
-        #     N = 3, 
-        #     Wn = 60,
-        #     btype='low',
-        #     analog=False,
-        #     ftype='butter',
-        #     fs = (1/self.Ts)
-        # )
+        rospy.sleep(0.1)
         self.d_filter = None
         self.e_filter = None
         self.currents_filter = None
-        self.pid = PID1D(KP, KI, KD, 
-                         INTEGRATOR_WINDUP_LIMIT, 
-                         CLEGG_INTEGRATOR,
-                         publish_states=True,
-                         state_pub_topic="/pid1d_states")
+
+        self.controller = controllers.IntegralLQR(A, B, Q, R, Qi, dt=T_controller)
+        self.control_input_pub = rospy.Publisher("/oct_levitation/linear_1d_controller/control_input", Float64, queue_size=10)
+
         self.last_time = rospy.Time.now().to_sec()
         self.__first_time = True
-        self.current_timer = rospy.Timer(rospy.Duration(self.Ts), self.current_callback)
+        self.last_z = 0.0
+        self.current_timer = rospy.Timer(rospy.Duration(T_controller), self.current_callback)
 
     def current_callback(self, event):
         if self.__first_time:
             self.__first_time = False
+            dipole_tf = self.__tf_buffer.lookup_transform(self.vicon_frame, "vicon/world", rospy.Time())
+            self.last_z = dipole_tf.transform.translation.z
             self.last_time = rospy.Time.now().to_sec()
-        dt = rospy.Time.now().to_sec() - self.last_time 
-        self.last_time = rospy.Time.now().to_sec()
+            return
         # This is where the control loop runs.
         dipole_tf = self.__tf_buffer.lookup_transform(self.vicon_frame, "vicon/world", rospy.Time())
         dipole_position = np.array([dipole_tf.transform.translation.x,
                                     dipole_tf.transform.translation.y,
                                     dipole_tf.transform.translation.z])
-        fz = self.pid.update(self.home_z, dipole_position[2], dt)
+        # dt = rospy.Time.now().to_sec() - self.last_time 
+        dt = T_controller
+        z_dot = (dipole_position[2] - self.last_z) / dt
+        self.last_z = dipole_position[2]
+        y = np.array([[dipole_position[2], z_dot]]).T
+        r = np.array([[self.home_z, 0]]).T
+        fz = self.controller.update(r, y, dt)
+        fz = fz[0,0]
         fz += common.Constants.g * self.dipole_mass # compensate for gravity
+        self.control_input_pub.publish(Float64(data=fz))
+        rospy.loginfo("Control Input: {}".format(fz))
+        self.last_time = rospy.Time.now().to_sec()
         M = common.get_magnetic_interaction_matrix(dipole_tf, 
                                                    self.dipole_strength,
                                                    self.dipole_axis)

scripts/utils/dynamics_simulator.py
@@ -15,7 +15,7 @@ NORTH_DOWN = True
 M_FRAME = 2.9e-3 # kg
 M_SMALL_MAGNET = 2.25e-3 # kg
 B = 0.0 # damping coefficient (friction)
-X0 = np.array([0.03, 0]) # initial state, m
+X0 = np.array([0.03, 0]) # initial state: x, x_dot
 DIPOLE_AXIS = np.array([0, 0, 1])
 L_BOUNDS = np.array([-0.05, -np.inf])
 U_BOUNDS = np.array([0.05, np.inf])
@@ -32,15 +32,18 @@ class DynamicsSimulator:
         self.vicon_pub = rospy.Publisher(self.vicon_frame, TransformStamped, queue_size=10)
 
         self.last_command_recv = 0
+        self.last_commmand_timeout = 0.2
+        self.__last_command_recv_time = 0.0
+        self.__first_command = True
+        self.__last_command_warning_sent = False
         self.last_time = rospy.Time.now().to_sec()
         self.__first_call = True
-        self.dt = 0
 
         self.__tf_msg.header.frame_id = self.world_frame
         self.__tf_msg.child_frame_id = self.vicon_frame
         m = common.NarrowRingMagnet.m * MAGNET_STACK_SIZE + \
             common.NarrowRingMagnet.mframe
-        self.sys = ZLevitatingMassSystem(m, B, X0, L_BOUNDS, U_BOUNDS)
+        self.sys = ZLevitatingMassSystem(m, B, True, self.Ts, X0, L_BOUNDS, U_BOUNDS)
 
         self.currents_sub = rospy.Subscriber("/tnb_mns_driver/des_currents_reg", DesCurrentsReg, self.currents_callback, queue_size=1)
         self.mag_model = common.OctomagCalibratedModel(calibration_type="legacy_yaml", 
@@ -61,12 +64,12 @@ class DynamicsSimulator:
         self.dipole_position = np.array([0, 0, X0[0]])
     
     def timer_callback(self, event):
-        if self.__first_call:
-            self.__first_call = False
-            self.last_time = rospy.Time.now().to_sec()
-        self.dt = rospy.Time.now().to_sec() - self.last_time
-        self.last_time = rospy.Time.now().to_sec()
-        self.sys.update(self.last_command_recv, self.dt)
+        if rospy.Time.now().to_sec() - self.__last_command_recv_time > self.last_commmand_timeout:
+            if not self.__last_command_warning_sent:
+                self.__last_command_warning_sent = True
+                rospy.logwarn("No command received in the last %.2f seconds." % self.last_commmand_timeout)
+                self.last_command_recv = 0
+        self.sys.update(self.last_command_recv, None)
         state = self.sys.get_state()
         self.dipole_position = np.array([0, 0, state[0]])
         self.__tf_msg.header.stamp = rospy.Time.now()
@@ -75,6 +78,9 @@ class DynamicsSimulator:
         self.vicon_pub.publish(self.__tf_msg)
 
     def currents_callback(self, msg: DesCurrentsReg):
+        if self.__first_command:
+            self.__first_command = False
+            self.__last_command_recv_time = rospy.Time.now().to_sec()
         M = common.get_magnetic_interaction_matrix(self.__tf_msg, 
                                                    common.NarrowRingMagnet.dps,
                                                    DIPOLE_AXIS)
@@ -82,6 +88,10 @@ class DynamicsSimulator:
         wrench = M @ A @ np.array(msg.des_currents_reg)
         fz = wrench[5]
         self.last_command_recv = fz
+        self.__last_command_recv_time = rospy.Time.now().to_sec()
+        if self.__last_command_warning_sent:
+            rospy.loginfo("Command received again.")
+            self.__last_command_warning_sent = False
     
     def run(self):
         self.timer = rospy.Timer(rospy.Duration(self.Ts), self.timer_callback)

src/oct_levitation/controllers.py
@@ -7,6 +7,7 @@ from scipy.linalg import block_diag
 import oct_levitation.common as common
 import oct_levitation.filters as filters
 from oct_levitation.msg import PID1DState
+from typing import Optional
 
 import rospy
 
@@ -70,7 +71,7 @@ class PID1D(ControllerInteface):
             self.state_pub.publish(state_msg)
         return u
     
-class DiscreteIntegralLQR(ControllerInteface):
+class IntegralLQR(ControllerInteface):
 
     def __init__(self,
                  A: np.ndarray,
@@ -78,7 +79,8 @@ class DiscreteIntegralLQR(ControllerInteface):
                  Q: np.ndarray,
                  R: np.ndarray,
                  Qi: np.ndarray,
-                 dt: float,
+                 dt: Optional[float] = 1e-3,
+                 discretize: bool = True,
                  windup_lim: float = np.inf,
                  clegg_integrator: bool = False):
         """
@@ -100,6 +102,7 @@ class DiscreteIntegralLQR(ControllerInteface):
         self.B = B
         self.Q = Q
         self.R = R
+        self.discretize = discretize
         
         # Designing a state augmented integral LQR controller
         A_aug = np.block([[A, np.zeros((A.shape[0], A.shape[0]))],
@@ -108,8 +111,11 @@ class DiscreteIntegralLQR(ControllerInteface):
         Q_aug = block_diag(Q, Qi)
 
         # Performing an exact ZOH discretization of the augmented system
-        Ad, Bd, Cd, Dd, dt = signal.cont2discrete((A_aug, B_aug, np.eye(A_aug.shape[0]), 0), dt=dt, method='zoh')
-        self.lqr_out = ct.dlqr(Ad, Bd, Q_aug, R)
+        if self.discretize:
+            Ad, Bd, Cd, Dd, dt = signal.cont2discrete((A_aug, B_aug, np.eye(A_aug.shape[0]), 0), dt=dt, method='zoh')
+            self.lqr_out = ct.dlqr(Ad, Bd, Q_aug, R)
+        else:
+            self.lqr_out = ct.lqr(A_aug, B_aug, Q_aug, R)
         self.K = self.lqr_out[0]
 
         self.windup_lim = windup_lim
@@ -126,4 +132,5 @@ class DiscreteIntegralLQR(ControllerInteface):
         e_integral = np.clip(self.e_integral, -self.windup_lim, self.windup_lim)
         u = -self.K @ np.vstack([e, e_integral])
         self.e_prev = e
+        return u
 

src/oct_levitation/dynamics.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy as sci
+import scipy.signal as sig
 import rospy
 import control as ct
 import tf2_ros
@@ -8,7 +8,7 @@ import oct_levitation.common as common
 
 from time import perf_counter
 from geometry_msgs.msg import TransformStamped
-from typing import Union
+from typing import Union, Optional
 
 class DynamicalSystemInterface:
 
@@ -33,6 +33,8 @@ class ZLevitatingMassSystem(DynamicalSystemInterface):
     def __init__(self, 
                  m: float, 
                  b: float,
+                 discretize: bool = True,
+                 dt: Optional[float] = 1e-3,
                  x0: np.ndarray = np.array([0, 0]),
                  lbounds: float = np.array([-np.inf, -np.inf]),
                  ubounds: float = np.array([np.inf, np.inf])):
@@ -42,16 +44,26 @@ class ZLevitatingMassSystem(DynamicalSystemInterface):
         """
         self.m = m
         self.b = b
-        self.A = np.array([[0, 1], [0, -b/m]])
-        self.B = np.array([[0], [1/m]])
+        A = np.array([[0, 1], [0, -b/m]])
+        B = np.array([[0], [1/m]])
+        self.discretize = discretize
+        if discretize:
+            Ad, Bd, Cd, Dd, dt = sig.cont2discrete((A, B, np.eye(2), 0), dt, method='zoh')
+            self.A = Ad
+            self.B = Bd
+        else:
+            self.A = A
+            self.B = B
         self.x0 = x0
         self.x = x0
         self.lbounds = lbounds
         self.ubounds = ubounds
 
-    def update(self, u: Union[np.ndarray, float], dt: float):
-        self.x = self.x + dt * (self.A @ self.x.reshape(2,1) + self.B * (u - self.m * common.Constants.g)).flatten()
-        self.x = np.clip(self.x, self.lbounds, self.ubounds)
+    def update(self, u: Union[np.ndarray, float], dt: Optional[bool]):
+        if self.discretize:
+            self.x = (self.A @ self.x.reshape(2,1) + self.B * (u - self.m * common.Constants.g)).flatten()
+        else:
+            self.x = self.x + dt * (self.A @ self.x.reshape(2,1) + self.B * (u - self.m * common.Constants.g)).flatten()
         if self.x[0] > self.ubounds[0]:
             self.x[0] = self.ubounds[0]
             self.x[1] = 0